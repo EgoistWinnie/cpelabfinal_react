6ae210e876667715c0cfd0d7e0b0faa868823093
winnieinyourarea <thepiggywin2000@gmail.com>
2020-04-17 19:26:16 +0700

    Updates

---
+++ precache-manifest.98fe3144b30c0c4f63541bf153fcc115.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da780efa73fa400e473da3fd9978e88d",
+    "url": "/cpelabfinal/index.html"
+  },
+  {
+    "revision": "1fc9ef47de18e2625969",
+    "url": "/cpelabfinal/static/css/2.d8011d99.chunk.css"
+  },
+  {
+    "revision": "494a68bfb6603dd0e74c",
+    "url": "/cpelabfinal/static/css/main.5857834a.chunk.css"
+  },
+  {
+    "revision": "1fc9ef47de18e2625969",
+    "url": "/cpelabfinal/static/js/2.a9368edc.chunk.js"
+  },
+  {
+    "revision": "494a68bfb6603dd0e74c",
+    "url": "/cpelabfinal/static/js/main.9622432d.chunk.js"
+  },
+  {
+    "revision": "00fe77dc45a89f1fd802",
+    "url": "/cpelabfinal/static/js/runtime~main.3672ef9f.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/cpelabfinal/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/cpelabfinal/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/cpelabfinal/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/cpelabfinal/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "62604f2d418c3498f00f1627f508f17d",
+    "url": "/cpelabfinal/static/media/PondD.62604f2d.jpg"
+  },
+  {
+    "revision": "7a927ef85542997c299a9caaa2971332",
+    "url": "/cpelabfinal/static/media/Winnie.7a927ef8.jpg"
+  },
+  {
+    "revision": "743b814a78807f38c192fccfba9e20de",
+    "url": "/cpelabfinal/static/media/facewin.743b814a.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/cpelabfinal/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/cpelabfinal/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/cpelabfinal/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/cpelabfinal/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/cpelabfinal/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/cpelabfinal/static/media/sidebar-4.7bc259fd.jpg"
+  }
+]);
